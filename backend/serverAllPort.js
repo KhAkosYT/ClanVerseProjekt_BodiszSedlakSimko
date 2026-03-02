bfe6ab3884326ae2769aca2dd31bbda376f55682
KhAkosYT <sedlakakos45@gmail.com>
2026-03-02 09:42:04 +0100

    Minden porton futó szerver létrehozása, package.json kibővítése ehhez
    Ha a parancssorban ezt a serverAllPort fájlt indítjuk, akkor minden porton figyel a backend, nem csak a localhost-on: így meg lehet nyitni bármilyen eszközről azonos hálózaton IP cím segítségével.
    pl.: 192.168.1.11:3000 a backend portja

---
+++ backend/serverAllPort.js
@@ -0,0 +1,73 @@
+//! Szerver indítása minden IP-címről
+
+//adatbázis impor-ok
+const sequelize = require('./database/sequelize');
+
+//utils import-ok
+const config = require('./utils/config');
+
+
+//seed import(-ok)
+const seedGames = require('./seeds/gamesSeed');
+const seedUsers = require('./seeds/usersSeed');
+const seedClans = require('./seeds/clansSeed');
+const seedClanMembers = require('./seeds/clanMembersSeed');
+const seedMessages = require('./seeds/messagesSeed');
+
+//App import-ok
+const express = require('express');
+const cors = require('cors');
+
+//routerek
+const usersRouter = require('./routes/user.routes');
+const clansRouter = require('./routes/clans.routes');
+const gamesRouter = require('./routes/game.routes');
+const messagesRouter = require('./routes/messages.routes');
+const indexRouter = require('./routes/index.routes');
+const adminRouter = require('./routes/admin.routes');
+
+const app = express();
+app.use(express.json());
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true
+    })
+);
+
+
+app.use('/uploads', express.static('uploads'));
+
+sequelize.sync().then(async () => {
+    console.log('Az adatbázis szinkronizálva.');
+
+    //! Első futtatáskor kell csak
+    await seedGames();
+    await seedUsers();
+    await seedClans();
+    await seedClanMembers();
+    await seedMessages();
+}).catch((err) => {
+    console.error('Hiba az adatbázis szinkronizálásakor:', err);
+});
+
+//router-ek haszálata
+app.use('/api/users', usersRouter);
+
+app.use('/api/clans', clansRouter)
+
+app.use('/api/games', gamesRouter)
+
+app.use('/api/messages', messagesRouter)
+
+app.use('/api', indexRouter)
+
+app.use('/api/admin', adminRouter)
+
+
+app.listen(config.serverPort, '0.0.0.0', () => {
+    console.log(`Szerver elindult minden IP-címről!`);
+});
+    
